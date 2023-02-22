08e5ca8edb4743432f664949dd128b579cd12953
Mark Visschers <visschers.m@gmail.com>
2023-02-22 10:48:28 +0100

    Added first test

--- tests/Utilities.Tests/GenericFactory.UnitTests.cs
+++ tests/Utilities.Tests/GenericFactory.UnitTests.cs
@@ -1,29 +1,36 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Matr.Utilities;
+using FluentAssertions;
 
-namespace Matr.Utilities.Tests;
-
-[TestClass]
-public class UnitTest1
+namespace Matr.Utilities.Tests
 {
-    protected readonly GenericFactory factory = new GenericFactory();
-
-    [ClassInitialize]
-    public static void ClassInit()
+    [TestClass]
+    public class UnitTest1
     {
+        private readonly GenericFactory factory = new GenericFactory();
 
+        // [ClassInitialize]
+        // public static void ClassInit()
+        // {
+
+        // }
+
+        // [ClassCleanup]
+        // public static void ClassCleanup()
+        // {
+
+        // }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            factory.RegisterOrReplace<ITestDependency>(new TestDependency());
+
+            var testService = factory.Create<TestService>();
+
+            testService.Should().NotBeNull();
+        }
+
+        // GetRegisteredServices
     }
-
-    [ClassCleanup]
-    public static void ClassCleanup()
-    {
-
-    }
-
-    [TestMethod]
-    public void TestMethod1()
-    {
-        // factory.RegisterOrReplace(new Mock<ITestDependency>().Object);
-    }
-
-    // GetRegisteredServices
 }
